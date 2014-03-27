e57988ef0a7426ef89eb251ad7cb0560191537ae
anton <AntonAbramov1990@gmail.com>
2014-03-27 23:06:23 +0200

    html structure, sass modules, header navigation, header top section with logo img and ocntacts

--- js/init.js
+++ js/init.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+	$("#nav").addClass("js").before('<div id="menu">☰</div>');
+	$("#menu").click(function(){
+		$("#nav").toggle();
+	});
+});
 
-
+$(window).resize(function(){
+	if(window.innerWidth > 768) {
+		$("#nav").removeAttr("style");
+	}
 });
